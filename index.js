85d238845176654dd32433a45128cf37bc465c68
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-04-28 16:30:34 +0400

    add routes for signup, signin pages

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
+app.get('/signup', (req, res) => {
+    res.sendFile(path.resolve("./pages/signup.html"))
+})
+app.get('/signin', (req, res) => {
+    res.sendFile(path.resolve("./pages/signin.html"))
+})
 
 app.listen(5000, () => {
     console.log('server is up...')
