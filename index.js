04c566de1f6324c4fb5dfeaf9a6e693b686fde03
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-04-28 17:23:41 +0400

    finish sign-in process

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import userModel from './model/index.js'
 
 mongoose.connect('mongodb+srv://mern02:mern02@cluster0.otbbzte.mongodb.net/?retryWrites=true&w=majority')
 const app = express()
+app.use(express.urlencoded())
 
 app.get('/signup', (req, res) => {
     res.sendFile(path.resolve("./pages/signup.html"))
@@ -28,7 +29,35 @@ app.post("/signup", upload.single('image'), async (req, res) => {
         image: secure_url
     }
     userModel.create(newUser)
-    res.send('has been created successfully!')
+    return res.redirect('/signin')
+})
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(404).send('user not found!')
+    }
+
+    const passwordsSame = await bcrypt.compare(password, user.password)
+    if (!passwordsSame) {
+        return res.status(401).send('password is wrong!')
+    }
+
+    res.redirect('/profile')
+})
+
+app.get('/profile', async (req, res) => {
+    const email = 'alion@gmail.com'
+    const user = await userModel.findOne({ email })
+    res.send(`
+    
+    <h1>Username: ${user.username}</h1>
+    <h1>Email: ${user.email}</h1>
+    <img src="${user.image}" />
+    
+    `)
 })
 
 app.listen(5000, () => {
