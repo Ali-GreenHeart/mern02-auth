89f19950d89931872719985ec9833431becbcc8e
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-01 16:48:31 +0400

    add jwt with page (html)

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import cloudinary from './cloudinary.js'
 import upload from './multer.js'
 import bcrypt from 'bcrypt'
 import userModel from './model/index.js'
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = 'RUTXCVBKLBG&IDCUB7r5y'
 
 mongoose.connect('mongodb+srv://mern02:mern02@cluster0.otbbzte.mongodb.net/?retryWrites=true&w=majority')
 const app = express()
@@ -35,7 +38,7 @@ app.post("/signup", upload.single('image'), async (req, res) => {
 app.post('/signin', async (req, res) => {
     const { email, password } = req.body
     const user = await userModel.findOne({ email })
-
+    console.log(email)
     if (!user) {
         return res.status(404).send('user not found!')
     }
@@ -45,19 +48,32 @@ app.post('/signin', async (req, res) => {
         return res.status(401).send('password is wrong!')
     }
 
-    res.redirect('/profile')
+    const token = jwt.sign({ email }, JWT_SECRET)
+    return res.redirect(`/profile/${token}`)
 })
 
-app.get('/profile', async (req, res) => {
-    const email = 'alion@gmail.com'
-    const user = await userModel.findOne({ email })
-    res.send(`
-    
-    <h1>Username: ${user.username}</h1>
-    <h1>Email: ${user.email}</h1>
-    <img src="${user.image}" />
-    
-    `)
+app.get('/profile/:token', async (req, res) => {
+    // const token = req.headers?.authorization?.split(" ")[1]
+    const { token } = req.params
+    if (!token) {
+        return res.status(401).send("Token yoxdur!")
+    }
+
+    jwt.verify(token, JWT_SECRET, async (err, data) => {
+        if (err) {
+            return res.send('token yanlisdir')
+        } else {
+            const { email } = data
+            const user = await userModel.findOne({ email })
+            return res.send(`
+                <h1>Username: ${user.username}</h1>
+                <h1>Email: ${user.email}</h1>
+                <img src="${user.image}" />
+            `)
+        }
+    })
+
+
 })
 
 app.listen(5000, () => {
